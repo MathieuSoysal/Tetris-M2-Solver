eedad0c99be8c416cd1b13200b7f18d26ac40dd9
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-05-13 10:56:27 +0000

    Add generateBoardFromString

--- src/main/java/io/github/mathieusoysal/Board.java
+++ src/main/java/io/github/mathieusoysal/Board.java
@@ -3,6 +3,7 @@ package io.github.mathieusoysal;
 public class Board {
     private static final int HEIGHT = 4;
     private static final int WIDTH = 7;
+    private static final int SIZE = HEIGHT * WIDTH;
     private static final char EMPTY = '_';
     private static final char FILLED = '#';
 
@@ -21,5 +22,18 @@ public class Board {
             stringBuilder.append("\n");
         }
         return stringBuilder.toString();
+    }
+
+    public static long generateBoardFromString(String boardString) {
+        long board = 0b0L;
+        for (int rowIndex = 0; rowIndex < HEIGHT; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < WIDTH; columnIndex++) {
+                int index = rowIndex * WIDTH + columnIndex;
+                if (index < boardString.length() && (boardString.charAt(index) == FILLED)) {
+                    board |= 1L << (SIZE - (index + 1));
+                }
+            }
+        }
+        return board;
     }
 }
--- src/test/java/io/github/mathieusoysal/BoardTest.java
+++ src/test/java/io/github/mathieusoysal/BoardTest.java
@@ -18,4 +18,19 @@ class BoardTest {
         assertEquals("#______\n_______\n_______\n_______\n", Board.toString(board));
     }
 
+    @Test
+    void testGenerateBoardFromString_withEmptyBoard() {
+        String boardString = "_______\n_______\n_______\n_______\n";
+        long excpectedBoard = 0b0L;
+        assertEquals(excpectedBoard, Board.generateBoardFromString(boardString));
+    }
+
+    @Test
+    void testGenerateBoardFromString_withFilledBoard() {
+        String boardString = "#______\n_______\n_______\n_______\n";
+        long excpectedBoard = 0b1000000_0000000_0000000_0000000L;
+        String excpetedBitString = Long.toBinaryString(excpectedBoard);
+        assertEquals(excpetedBitString, Long.toBinaryString(Board.generateBoardFromString(boardString)));
+    }
+
 }
