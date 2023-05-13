9c0eb07cb1772884795f6bee078ef0b8bacb40c3
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-05-13 11:29:14 +0000

    Update pitest plugin

--- src/test/java/io/github/mathieusoysal/BoardTest.java
+++ src/test/java/io/github/mathieusoysal/BoardTest.java
@@ -1,32 +1,32 @@
 package io.github.mathieusoysal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class BoardTest {
+public class BoardTest {
 
     @Test
-    void testBpard_withEmptyBoard() {
+    public void testBpard_withEmptyBoard() {
         long board = 0b0L;
         assertEquals("_______\n_______\n_______\n_______\n", Board.toString(board));
     }
 
     @Test
-    void testBoard_withFilledBoard() {
+    public void testBoard_withFilledBoard() {
         long board = 0b1000000_0000000_0000000_0000000L;
         assertEquals("#______\n_______\n_______\n_______\n", Board.toString(board));
     }
 
     @Test
-    void testGenerateBoardFromString_withEmptyBoard() {
+    public void testGenerateBoardFromString_withEmptyBoard() {
         String boardString = "_______\n_______\n_______\n_______\n";
         long excpectedBoard = 0b0L;
         assertEquals(excpectedBoard, Board.generateBoardFromString(boardString));
     }
 
     @Test
-    void testGenerateBoardFromString_withFilledBoard() {
+    public void testGenerateBoardFromString_withFilledBoard() {
         String boardString = "#______\n_______\n_______\n_______\n";
         long excpectedBoard = 0b1000000_0000000_0000000_0000000L;
         String excpetedBitString = Long.toBinaryString(excpectedBoard);
