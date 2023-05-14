b28f5f1afa59bf4be0c7497555ccc5cb5e2a0a93
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-05-14 10:06:39 +0000

    Add Z

---
+++ src/main/java/io/github/mathieusoysal/puzzle_piece/Z.java
@@ -0,0 +1,33 @@
+package io.github.mathieusoysal.puzzle_piece;
+
+public class Z implements PuzzlePieceInterface {
+    private static final long SHAPE = 0b1100000_0110000_0000000_0000000L;
+    private static final long[] allPossiblePlacements = new long[] {
+            0b1100000_0110000_0000000_0000000L,
+            0b0110000_0011000_0000000_0000000L,
+            0b0011000_0001100_0000000_0000000L,
+            0b0001100_0000110_0000000_0000000L,
+            0b0000110_0000011_0000000_0000000L,
+            0b0000000_1100000_0110000_0000000L,
+            0b0000000_0110000_0011000_0000000L,
+            0b0000000_0011000_0001100_0000000L,
+            0b0000000_0001100_0000110_0000000L,
+            0b0000000_0000110_0000011_0000000L,
+            0b0000000_0000000_1100000_0110000L,
+            0b0000000_0000000_0110000_0011000L,
+            0b0000000_0000000_0011000_0001100L,
+            0b0000000_0000000_0001100_0000110L,
+            0b0000000_0000000_0000110_0000011L
+    };
+
+    @Override
+    public long getShape() {
+        return SHAPE;
+    }
+
+    @Override
+    public long[] getAllPossiblePlacements() {
+        return allPossiblePlacements;
+    }
+
+}
---
+++ src/test/java/io/github/mathieusoysal/puzzle_piece/ZTest.java
@@ -0,0 +1,16 @@
+package io.github.mathieusoysal.puzzle_piece;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import io.github.mathieusoysal.Board;
+
+public class ZTest {
+
+    @Test
+    public void testZ_getShape() {
+        assertEquals("##_____\n_##____\n_______\n_______\n", Board.toString(new Z().getShape()));
+    }
+
+}
