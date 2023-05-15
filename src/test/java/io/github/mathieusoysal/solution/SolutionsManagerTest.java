17725542bc02a1ec287372217030d23521becfd1
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-05-15 16:55:55 +0000

    Add test to solutionManager

--- src/test/java/io/github/mathieusoysal/solution/SolutionsManagerTest.java
+++ src/test/java/io/github/mathieusoysal/solution/SolutionsManagerTest.java
@@ -1,8 +1,12 @@
 package io.github.mathieusoysal.solution;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import io.github.mathieusoysal.puzzle_piece.PuzzlePieces;
 
 public class SolutionsManagerTest {
 
@@ -11,6 +15,26 @@ public class SolutionsManagerTest {
         long nbUsesOfEachPieces = SolutionsManager.bitMapperOfNbUse(1, 1, 1, 1, 1, 1);
         long expected = 0b001_001_001_001_001_001L;
         assertEquals(expected, nbUsesOfEachPieces);
+    }
+
+    @Test
+    public void testAddSolution_withOneSolution() {
+        SolutionsManager solutionsManager = new SolutionsManager(PuzzlePieces.MINIS_SQUARE);
+        assertTrue(solutionsManager.addSolution(0, 0, 1, 1, 1, 1, 1, 1));
+    }
+
+    @Test
+    public void testAddSolution_withMutipleAdd() {
+        SolutionsManager solutionsManager = new SolutionsManager(PuzzlePieces.MINIS_SQUARE);
+        solutionsManager.addSolution(0, 0, 1, 1, 1, 1, 1, 1);
+        assertTrue(solutionsManager.addSolution(0, 0, 1, 2, 1, 1, 1, 1));
+    }
+
+    @Test
+    public void testAddSolution_withSameAdd_shouldReturnFals() {
+        SolutionsManager solutionsManager = new SolutionsManager(PuzzlePieces.MINIS_SQUARE);
+        solutionsManager.addSolution(0, 0, 1, 1, 1, 1, 1, 1);
+        assertFalse(solutionsManager.addSolution(0, 0, 1, 1, 1, 1, 1, 1));
     }
 
 }
