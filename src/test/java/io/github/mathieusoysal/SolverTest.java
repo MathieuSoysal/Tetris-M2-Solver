5d035d6feada879cf8688dac49ddfef537593667
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-05-16 13:49:30 +0000

    WIP: Solver

--- src/test/java/io/github/mathieusoysal/SolverTest.java
+++ src/test/java/io/github/mathieusoysal/SolverTest.java
@@ -7,8 +7,8 @@ import io.github.mathieusoysal.puzzle_piece.PuzzlePieces;
 
 public class SolverTest {
 
-    @Test
-    @Ignore
+    // @Test
+    // @Ignore
     public void testSolve() {
         long heapsize = Runtime.getRuntime().totalMemory();
         System.out.println("heapsize is :: " + heapsize * .000000000125);
