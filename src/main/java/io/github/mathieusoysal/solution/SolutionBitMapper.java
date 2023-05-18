5cbdf7c967a68d142fec659a545980d976298e98
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-05-18 16:16:44 +0000

    Add PuzzlePiece mapping into SolutionBitMapper

--- src/main/java/io/github/mathieusoysal/solution/SolutionBitMapper.java
+++ src/main/java/io/github/mathieusoysal/solution/SolutionBitMapper.java
@@ -1,20 +1,30 @@
 package io.github.mathieusoysal.solution;
 
+import java.util.EnumMap;
+
+import io.github.mathieusoysal.puzzle_piece.PuzzlePieces;
+
 public class SolutionBitMapper {
     private static final int BOARD_MASK = 0b1111111_1111111_1111111_1111111;
 
-
-    public static int mapBoardAndPuzzlePieceIntoInt(int board, int puzzlePiece) {
-        return board | puzzlePiece << 28;
+    public static int mapBoardAndPuzzlePieceIntoInt(int board, PuzzlePieces puzzlePiece) {
+        return mapBoardAndPuzzlePieceIntoInt(board, puzzlePiece.ordinal());
     }
 
+    static int mapBoardAndPuzzlePieceIntoInt(int board, int puzzlePiece) {
+        return board | puzzlePiece << 28;
+    }
 
     public static int getBoardFromSolutionBitMap(int solutionBitMap) {
         return solutionBitMap & BOARD_MASK;
     }
 
-    public static int getPuzzlePieceIndexFromSolutionBitMap(int solutionBitMap) {
+    public static PuzzlePieces getPuzzlePieceFromSolutionBitMap(int solutionBitMap) {
+        return PuzzlePieces.values()[getPuzzlePieceIndexFromSolutionBitMap(solutionBitMap)];
+    }
+
+    static int getPuzzlePieceIndexFromSolutionBitMap(int solutionBitMap) {
         return solutionBitMap >>> 28;
     }
-    
+
 }
--- src/test/java/io/github/mathieusoysal/solution/SolutionBitMapperTest.java
+++ src/test/java/io/github/mathieusoysal/solution/SolutionBitMapperTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import io.github.mathieusoysal.puzzle_piece.PuzzlePieces;
+
 public class SolutionBitMapperTest {
 
     @Test
@@ -70,5 +72,35 @@ public class SolutionBitMapperTest {
         int expected = 6;
         int actual = SolutionBitMapper.getPuzzlePieceIndexFromSolutionBitMap(solutionBitMap);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetPuzzlePieceFromSolutionBitMap() {
+        int solutionBitMap = 0b101_0000000_0000000_0000000_0000000;
+
+        int expected = 5;
+        int actual = SolutionBitMapper.getPuzzlePieceFromSolutionBitMap(solutionBitMap).ordinal();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMapBoardAndPuzzlePieceIntoInt2() {
+        int board = 0b1111111_1100111_1100111_1111111;
+        var puzzlePiece = PuzzlePieces.Z;
+
+        int expected = 0b1_1111111_1100111_1100111_1111111;
+        int actual = SolutionBitMapper.mapBoardAndPuzzlePieceIntoInt(board, puzzlePiece);
+        assertEquals(Integer.toBinaryString(expected), Integer.toBinaryString(actual));
+    }
+
+    @Test
+    public void testMapBoardAndPuzzlePieceIntoInt3() {
+        int board = 0b1111111_1100111_1100111_1111111;
+        var puzzlePiece = PuzzlePieces.STICK;
+
+        int actual = SolutionBitMapper.mapBoardAndPuzzlePieceIntoInt(board, puzzlePiece);
+        var actualPuzzlePiece = SolutionBitMapper.getPuzzlePieceFromSolutionBitMap(actual);
+        assertEquals(puzzlePiece, actualPuzzlePiece);
     }
 }
